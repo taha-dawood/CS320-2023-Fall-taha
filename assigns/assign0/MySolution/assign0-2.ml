cec96c0b02384862f961834340237c6cb955506b
Taha Dawood <114628075+taha-dawood@users.noreply.github.com>
2023-09-13 20:47:12 -0400

    working

---
+++ assigns/assign0/MySolution/assign0-2.ml
@@ -0,0 +1,16 @@
+let isPrime n0 =
+  if n0 <= 1 then
+    false
+  else if n0 <= 3 then
+    true
+  else
+    let rec is_divisible_by d =
+      if d * d > n0 then
+        true
+      else if n0 mod d = 0 then
+        false
+      else
+        is_divisible_by (d + 2)
+    in
+    n0 mod 2 <> 0 && is_divisible_by 3
+;;
---
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -0,0 +1,27 @@
+let int2str i0 =
+  let rec convert_to_string n acc len =
+    if n = 0 then
+      if len = 0 then "0" else acc
+    else
+      let remainder = n mod 10 in
+      let digit_char = char_of_int (48 + remainder) in
+      let new_acc = String.make 1 digit_char in
+      let new_len = len + 1 in
+      let new_acc_len = String.length new_acc in
+      let combined_len = new_len + new_acc_len in
+      let combined_str = String.init combined_len (fun i ->
+        if i = 0 then '-'
+        else if i = 1 then ' '
+        else if i < new_acc_len + 2 then String.get new_acc (i - 2)
+        else String.get acc (i - new_acc_len - 2)
+      ) in
+      convert_to_string (n / 10) combined_str combined_len
+  in
+  if i0 < 0 then
+    (convert_to_string (-i0) "" 0)
+  else
+    convert_to_string i0 "" 0
+;;
+
+
+
